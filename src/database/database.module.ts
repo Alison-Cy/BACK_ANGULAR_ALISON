b9ea29e8c69fad1118e6b7334c042831dcca578b
Alison-Cy <avc.cumbajin@yavirac.edu.ec>
2024-05-18 15:15:35 -0500

    datbase-provider

---
+++ src/database/database.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { databaseProvider } from './database.providers';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+    providers:[...databaseProvider, ConfigService],
+    exports:[...databaseProvider]
+})
+export class DatabaseModule {}
